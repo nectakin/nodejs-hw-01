c562cb76f3b4a803adc9a4706adb481528365234
nectakin <kvs171005@gmail.com>
2024-08-08 17:44:00 +0300

    fix

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,14 +1,12 @@
-
 import fs from 'fs/promises';
-
 import { PATH_DB } from '../constants/contacts.js';
-
 import { createFakeContact } from '../utils/createFakeContact.js';
 
 export const addOneContact = async () => {
   const contacts = JSON.parse(await fs.readFile(PATH_DB));
 
-  fs.writeFile(
+
+  await fs.writeFile(
     PATH_DB,
     JSON.stringify([...contacts, createFakeContact()], null, 2),
   );
